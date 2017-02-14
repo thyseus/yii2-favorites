a2584cb51e3d7c695ee2b444298ae274334d4f7c
Thomas Baier <thomas.baier@haendlerbund.de>
2017-02-14 10:00:03 +0100

    fav buttons style

--- views/favorites/_button.php
+++ views/favorites/_button.php
@@ -15,12 +15,13 @@ if (!isset($target_attribute))
     $target_attribute = null;
 
 if ($favorite = Favorite::exists($model, $owner, $target))
-    echo Html::a(Yii::t('app', 'Remove favorite'),
+    echo Html::a(
+        '<span data-toggle="popover" title="Merkliste" data-content="Klicken und aus der Merkliste entfernen"><span class="fa fa-star-o ci-color"></span></span>',
         ['/favorites/favorites/delete', 'id' => $favorite->id],
         $htmlOptions);
 else
     echo Html::a(
-        Yii::t('app', 'Set as favorite'),
+        '<span data-toggle="popover" title="Merkliste" data-content="Klicken und zur Merkliste hinzufügen"><span class="fa fa-star ci-color"></span></span>',
         ['/favorites/favorites/create',
             'model' => $model,
             'target_id' => $target,
