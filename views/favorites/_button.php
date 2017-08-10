9ca0930ec8e1c5a70c542bcfdf059e35eaaa6194
Herbert Maschke <thyseus@gmail.com>
2017-08-10 12:08:47 +0200

    introduce ability to set custom tooltip labels

--- views/favorites/_button.php
+++ views/favorites/_button.php
@@ -4,23 +4,45 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\web\View;
 
-if (!isset($owner))
+if (!isset($owner)) {
     $owner = Yii::$app->user->id;
+}
 
-if (!isset($url))
+if (!isset($url)) {
     $url = null;
+}
 
-if (!isset($label_add) || $label_add === null)
+if (!isset($label_add) || $label_add === null) {
     $label_add = null;
+}
 
-if (!isset($label_remove) || $label_remove === null)
+if (!isset($label_remove) || $label_remove === null) {
     $label_remove = null;
+}
 
-if (!isset($target_attribute))
+if (!isset($label_tooltip_title_add) || $label_tooltip_title_add === null) {
+    $label_tooltip_title_add = Yii::t('app', 'Favorites');
+}
+
+if (!isset($label_tooltip_title_remove) || $label_tooltip_title_remove === null) {
+    $label_tooltip_title_add = Yii::t('app', 'Favorites');
+}
+
+if (!isset($label_tooltip_content_add) || $label_tooltip_content_add === null) {
+    $label_tooltip_content_add = Yii::t('app', 'Click to add to favorites');
+}
+
+if (!isset($label_tooltip_content_remove) || $label_tooltip_content_remove === null) {
+    $label_tooltip_content_remove = Yii::t('app', 'Click to remove favorite');
+}
+
+if (!isset($target_attribute)) {
     $target_attribute = null;
+}
 
-if(!isset($icon))
+if (!isset($icon)) {
     $icon = '<i class="glyphicon glyphicon-star"></i>';
+}
 
 $necessaryOptions = [
     'class' => 'favorite-button',
@@ -32,21 +54,24 @@ $necessaryOptions = [
     'style' => 'cursor: pointer;',
 ];
 
-if (!isset($htmlOptions))
+if (!isset($htmlOptions)) {
     $htmlOptions = $necessaryOptions;
-else
+} else {
     $htmlOptions = array_merge($htmlOptions, $necessaryOptions);
+}
 
 if ($favorite = Favorite::exists($model, $owner, $target)) {
-    echo Html::a(sprintf(
-        '<span data-toggle="popover" title="%s" data-content="%s">' . Yii::$app->getModule('favorites')->icon_inactive . $label_remove,
-        Yii::t('app', 'Favorites'),
-        Yii::t('app', 'Click to remove favorite')), null, array_merge($htmlOptions, ['data-status' => 'active', 'data-id' => $favorite->id]));
+    echo Html::a(sprintf('<span data-toggle="popover" title="%s" data-content="%s">%s%s',
+        $label_tooltip_title_remove,
+        $label_tooltip_content_remove,
+        Yii::$app->getModule('favorites')->icon_inactive,
+        $label_remove), null, array_merge($htmlOptions, ['data-status' => 'active', 'data-id' => $favorite->id]));
 } else {
-    echo Html::a(sprintf(
-        '<span data-toggle="popover" title="%s" data-content="%s">' . Yii::$app->getModule('favorites')->icon_active . $label_add,
-        Yii::t('app', 'Favorites'),
-        Yii::t('app', 'Click to add to favorites')), null, array_merge($htmlOptions, ['data-status' => 'inactive']));
+    echo Html::a(sprintf('<span data-toggle="popover" title="%s" data-content="%s">%s%s',
+        $label_tooltip_title_add,
+        $label_tooltip_content_add,
+        Yii::$app->getModule('favorites')->icon_active,
+        $label_add), null, array_merge($htmlOptions, ['data-status' => 'inactive']));
 }
 
 $url_create = Url::to(['/favorites/favorites/create']);
@@ -56,7 +81,7 @@ $this->registerJs("
     $('body').on('click', 'a.favorite-button', function(event) {
         data = {
              'model': $(this).data('model'),
-             'icon': '".$icon."',
+             'icon': '" . $icon . "',
              'target-id': $(this).data('target-id'),
              'url': $(this).data('url'),
              'target-attribute': $(this).data('target-attribute'),
